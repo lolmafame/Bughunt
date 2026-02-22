b4f7f68091d885ef599dbc56e016249c3a8eaf45
Gabriel Angelo Aguillon <aguillon.gabrielangelobscs2023@gmail.com>
2026-02-22 12:08:51 +0800

    spider animation
    animation for spider

--- Assets/SpiderDamage.cs
+++ Assets/SpiderDamage.cs
@@ -3,11 +3,23 @@ using UnityEngine;
 public class SpiderDamage : MonoBehaviour
 {
     public int damageAmount = 20;
+    private Animator animator;
+
+    void Start()
+    {
+       
+        animator = GetComponentInParent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+         
+            if (animator != null)
+                animator.SetTrigger("Attack");
+
+           
             PlayerHealth ph = other.GetComponent<PlayerHealth>();
             if (ph != null)
             {
