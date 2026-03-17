d1145407ed4dce66eaf6db737e2b34dfa363cc95
Jan Eunice Mitra <mitra.janeunicebscs2023@gmail.com>
2026-03-17 10:23:31 +0800

    added ui for complete and locked levels

---
+++ Assets/LevelState.cs
@@ -0,0 +1,6 @@
+public enum LevelState
+{
+    Closed,
+    Open,
+    Complete
+}
---
+++ Assets/LevelUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    public GameObject lockIcon;
+    public GameObject checkmarkIcon;
+    public Button levelButton;
+    public Image baseImage;
+    private Color lockedColor = new Color(1, 1, 1, 0.5f);
+    private Color completeColor = new Color (17, 64, 6, 0.2f);
+    private Color normalColor = Color.white;
+
+    public void SetState(LevelState state)
+    {
+        switch (state)
+        {
+            case LevelState.Closed:
+                lockIcon.SetActive(true);
+                checkmarkIcon.SetActive(false);
+                levelButton.interactable = false;
+                baseImage.color = lockedColor; 
+                break;
+
+            case LevelState.Open:
+                lockIcon.SetActive(false);
+                checkmarkIcon.SetActive(false);
+                levelButton.interactable = true;
+                baseImage.color = completeColor;
+                break;
+
+            case LevelState.Complete:
+                lockIcon.SetActive(false);
+                checkmarkIcon.SetActive(true);
+                levelButton.interactable = true;
+                baseImage.color = normalColor;
+                break;
+        }
+    }
+}
